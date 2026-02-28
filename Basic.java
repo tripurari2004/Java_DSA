27b4a81a7720bc8304efbbc7d5bfd50083445eb1
tripurari2268 <tripurarikumar18feb@gmail.com>
2026-02-28 07:27:10 +0530

    Java program demonstrating variables and calculations

---
+++ Basic.java
@@ -0,0 +1,12 @@
+public class Basic {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println(4+5);
+        System.out.println(true);
+
+        int physicsMarks = 85;
+        int chemistryMarks = 90;
+        int totalMarks = physicsMarks + chemistryMarks;
+        System.out.println("Total Marks: " + totalMarks);
+    }
+}
