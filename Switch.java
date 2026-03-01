7a5cf3a6d052b218a14a633c5306408bf8127ea0
tripurari2268 <tripurarikumar18feb@gmail.com>
2026-03-01 18:05:38 +0530

    Add Switch statement demo

---
+++ Switch.java
@@ -0,0 +1,32 @@
+public class Switch {
+    public static void main(String[] args) {
+        int day = 1;
+
+        switch (day) {
+            case 0:
+                System.out.print("Sunday");
+                break;
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursady");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            default:
+                System.out.print("Wrong Number");
+                break;
+        }
+    }
+}
