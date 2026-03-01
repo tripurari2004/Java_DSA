6568be7e04e37c1c5e6810d8113f1c2cf1614cae
tripurari2268 <tripurarikumar18feb@gmail.com>
2026-03-01 07:09:10 +0530

    Add Conditional.java demonstrating if-else statements, ternary operator, and switch statement with user input

---
+++ Conditional.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) {
+        //Conditional Statements
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+
+
+        System.out.print("Enter a number1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter a number2: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter a number3: ");
+        int num3 = scanner.nextInt();
+        int max = num1;
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+        System.out.println("The largest number is: " + max);
+
+        //Ternary Operator
+        System.out.print("Enter a number: ");
+        int number2 = scanner.nextInt();
+        String result = (number2 % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The number is: " + result);
+
+
+        //Switch Statement
+        System.out.print("Enter a grade (A-F): ");
+        char grade = scanner.next().charAt(0);
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'B':
+                System.out.println("Good job!");
+                break;
+            case 'C':
+                System.out.println("You passed.");
+                break;
+            case 'D':
+                System.out.println("You need to improve.");
+                break;
+            case 'F':
+                System.out.println("You failed.");
+                break;
+            default:
+                System.out.println("Invalid grade.");
+        }
+
+
+        //Close the scanner
+        scanner.close();
+    }
+}
