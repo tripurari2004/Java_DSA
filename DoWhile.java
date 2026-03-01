570382641b8d0bbcfd17c67051d5ccbd1d8317a8
tripurari2268 <tripurarikumar18feb@gmail.com>
2026-03-01 18:01:02 +0530

    Add DoWhile loop example for DSA fundamentals

---
+++ DoWhile.java
@@ -0,0 +1,8 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.print(counter);
+        }while(counter>1);
+    }
+}
