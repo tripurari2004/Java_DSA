150c4a1e5a86473bcc83ab0bd8399f1cb111ceed
tripurari2268 <tripurarikumar18feb@gmail.com>
2026-03-08 06:21:57 +0530

    Implement hollow rhombus pattern with nested loops

---
+++ hollowRhombus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class hollowRhombus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = a; j > i; j--) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= a; j++) {
+                if (i==1 || i==a || j==1 || j==a) {
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
