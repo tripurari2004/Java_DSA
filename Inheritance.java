1ccd034cebfcee832e5fdc70ed4237582518772e
tripurari2268 <tripurarikumar18feb@gmail.com>
2026-03-24 07:31:16 +0530

    demonstrate single and multi-level inheritance in Java

--- Inheritance.java
+++ Inheritance.java
@@ -2,6 +2,11 @@ public class Inheritance {
     public static void main(String[] args) {
         Fish shark = new Fish();
         shark.eat();
+
+        Dog d1 = new Dog();
+        d1.eat();
+        d1.legs = 4;
+        System.out.println(d1.legs);
     }
 }
 
@@ -16,10 +21,22 @@ class Animal{
     }
 }
 
+//Signle level inheritance
 //Derived class
 class Fish extends Animal{
     int fins;
     void swim(){
         System.out.println("Swim in water");
     }
+}
+
+
+//Multi level inheritance
+class Mammal extends Animal{
+    int legs;
+}
+
+
+class Dog extends Mammal{
+    String breed;
 }
